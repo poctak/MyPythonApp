06e78d82d3a1607d6cf616d205c536d921f4a136
Tomáš Počta <poctak@seznam.cz>
2025-07-24 16:11:22 +0200

    Změna textace

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ class HelloHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(b"Hello")
+        self.wfile.write(b"Hello Tom")
 
 if __name__ == "__main__":
     server = HTTPServer(("0.0.0.0", 80), HelloHandler)
